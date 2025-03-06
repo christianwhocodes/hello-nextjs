265c1f364db513737521d3f22c6c0e01b1faa550
vokewasike <vokewasike@gmail.com>
2025-03-06 13:17:27 +0000

    chore: add Tailwind Prettier plugin

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"], // 400 for normal, 700 for bold
+});
